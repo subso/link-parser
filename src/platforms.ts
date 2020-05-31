8bc076d1193998e6dc87547eea98286f1fad9b82
James Pine <jamie@Jamies-16.local>
2020-05-30 21:24:01 -0700

    forgot to copy over interfaces, added

--- src/platforms.ts
+++ src/platforms.ts
@@ -1,15 +1,17 @@
-export const platforms = {
-    twitch: {
-        key: 'twitch',
-        name: 'Twitch',
-        url: 'twitch.tv',
-        hex: '#6441A4',
-        wideThumb: true,
-        baseURLMatch: /http(?:s)?:\/\/(?:www\.)?twitch\.tv\//,
-        contentTypeMatches: {
-            clip: /(?:http(?:s)?:\/\/(?:www\.)?clips\.twitch\.tv\/([a-zA-Z0-9_]+))|(?:http(?:s)?:\/\/(?:www\.)?twitch\.tv\/[a-zA-Z0-9_]+\/clip\/([a-zA-Z0-9_]+))/,
-            vod: /http(?:s)?:\/\/(?:www\.)?twitch\.tv\/([a-zA-Z0-9_]+)/,
-            stream: /http(?:s)?:\/\/(?:www\.)?twitch\.tv\/([a-zA-Z0-9_]+)/,
-        },
+import { PlatformObject } from './interfaces';
+
+export const platforms: { [key: string]: PlatformObject } = {
+  twitch: {
+    key: 'twitch',
+    name: 'Twitch',
+    url: 'twitch.tv',
+    hex: '#6441A4',
+    wideThumb: true,
+    baseURLMatch: /http(?:s)?:\/\/(?:www\.)?twitch\.tv\//,
+    contentTypeMatches: {
+      clip: /(?:http(?:s)?:\/\/(?:www\.)?clips\.twitch\.tv\/([a-zA-Z0-9_]+))|(?:http(?:s)?:\/\/(?:www\.)?twitch\.tv\/[a-zA-Z0-9_]+\/clip\/([a-zA-Z0-9_]+))/,
+      vod: /http(?:s)?:\/\/(?:www\.)?twitch\.tv\/([a-zA-Z0-9_]+)/,
+      stream: /http(?:s)?:\/\/(?:www\.)?twitch\.tv\/([a-zA-Z0-9_]+)/,
     },
+  },
 };
