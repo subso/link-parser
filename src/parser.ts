042eeb33945e9ae22f803efeaf78e3375012e73e
James Pine <jamie@Jamies-16.local>
2020-05-30 21:19:37 -0700

    added comments to the parser

--- src/parser.ts
+++ src/parser.ts
@@ -1,20 +1,29 @@
 import { platforms } from './platforms';
 
-export function analyse(link: string) {
+export function analyse(link: string): string | undefined {
+    // if no string is provided just return undefined
     if (typeof link !== 'string') return undefined;
 
     for (let platformName in platforms) {
         // @ts-ignore
         let platformObj = platforms[platformName];
+        // if the link matches the baseURLMatch regex. It will stop at the first match out of all the platforms.
         if (link.match(platformObj.baseURLMatch)) {
+            // loop over the keys in the platform's "contentTypeMatches" property
             for (let contentTypeMatch in platformObj.contentTypeMatches) {
+                // each regex entry should be ordered so the most simple is LAST in the object
+                // the key of the entry into this object should be the content type ("clip" | "stream" | "video" etc)
                 let matched = link.match(platformObj.contentTypeMatches[contentTypeMatch]);
                 if (matched) {
+                    // extract key data and inject into platformObj
                     platformObj.extractedId = extract(matched);
                     platformObj.contentType = contentTypeMatch;
+
                     return platformObj;
                 }
             }
+            // this shouldn't get hit, in theory
+            return undefined;
         }
     }
 }
