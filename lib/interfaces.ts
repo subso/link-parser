889559add78c23c00c48a61dbd57ac4f2cd2ddd6
Jack Douglas <git.notify@jackdouglas.info>
2020-05-31 23:49:05 -0700

    Added Sotify

--- lib/interfaces.ts
+++ lib/interfaces.ts
@@ -13,6 +13,11 @@ export enum ContentType {
   'invitation',
   'event',
   'channel',
+  'artist',
+  'playlist',
+  'podcast',
+  'podcastEpisode',
+  'profile',
 }
 
 export interface PlatformObject {
--- tests/links.spec.ts
+++ tests/links.spec.ts
@@ -9,7 +9,7 @@ function testResult(result: any, extractedId: string, contentType: string) {
   expect(result.contentType).to.be.equal(contentType);
 }
 
-describe('Twitch link test', () => {
+describe('Twitch link tests', () => {
   it('should parse clips, streams and channel links', () => {
     // Clip Link One
     const clipLinkOne = analyse('https://www.twitch.tv/jamiepinelive0/clip/AttractiveRichAntelopeTheThing?filter=clips&range=7d&sort=time');
@@ -29,7 +29,7 @@ describe('Twitch link test', () => {
   });
 });
 
-describe('YouTube', () => {
+describe('YouTube link tests', () => {
   it('should prase youtube channel and video links', () => {
     // Channel
     const channelLink = analyse('https://www.youtube.com/channel/UCSa0hnM2n_yvOnecoQ2fgxA');
@@ -38,5 +38,37 @@ describe('YouTube', () => {
     // Video
     const videoLink = analyse('https://www.youtube.com/watch?v=LgqemiEqJtw');
     testResult(videoLink, 'LgqemiEqJtw', 'video');
+  });
+});
+
+describe('Spotify link tests', () => {
+  it('should parse spotify artist, track, album, playlist, podcast, postcast episode & user profile', () => {
+    // Artist
+    const artistLink = analyse('https://open.spotify.com/artist/0gxyHStUsqpMadRV0Di1Qt');
+    testResult(artistLink, '0gxyHStUsqpMadRV0Di1Qt', 'artist');
+
+    // Track
+    const trackLink = analyse('https://open.spotify.com/track/4uLU6hMCjMI75M1A2tKUQC?si=w_PjTGuCRsaRiRYga2pZ4g');
+    testResult(trackLink, '4uLU6hMCjMI75M1A2tKUQC', 'song');
+
+    // Album
+    const albumLink = analyse('https://open.spotify.com/album/09A5ujQAQP0zKYqxvrb1Wn?si=pcWSRL7fQOOCD2Tjcsp93A');
+    testResult(albumLink, '09A5ujQAQP0zKYqxvrb1Wn', 'album');
+
+    // Playlist
+    const playlistLink = analyse('https://open.spotify.com/playlist/19RcUUR4b9oxhcREqD8Xoq');
+    testResult(playlistLink, '19RcUUR4b9oxhcREqD8Xoq', 'playlist');
+
+    // Podcast
+    const podcastLink = analyse('https://open.spotify.com/show/6glBiNvUItycoTgwDgGTgL');
+    testResult(podcastLink, '6glBiNvUItycoTgwDgGTgL', 'podcast');
+
+    // Podcast Episode
+    const podcastEpisodeLink = analyse('https://open.spotify.com/episode/4pEP2gndFLNuDLWgEY2yEa');
+    testResult(podcastEpisodeLink, '4pEP2gndFLNuDLWgEY2yEa', 'podcastEpisode');
+
+    // User Profile
+    const profileLink = analyse('https://open.spotify.com/user/rickastley');
+    testResult(profileLink, 'rickastley', 'profile');
   });
 });
